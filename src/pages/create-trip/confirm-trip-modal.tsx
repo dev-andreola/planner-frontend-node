f33e6b1e4ff96f9904eef68dcfb940c9cc90c468
vtAndreola <dev.andreola@gmail.com>
2024-07-12 16:32:29 -0300

    feat(confirm-trip-modal): add dinamic info

--- src/pages/create-trip/confirm-trip-modal.tsx
+++ src/pages/create-trip/confirm-trip-modal.tsx
@@ -1,12 +1,16 @@
-import { User, X } from "lucide-react";
+import { AtSign, User, X } from "lucide-react";
 import { FormEvent } from "react";
 import { Button } from "../../components/button";
+import { DateRange } from "react-day-picker";
+import { format } from "date-fns";
 
 interface ConfirmTripModalProps {
   closeConfirmTripModal: () => void;
   createTrip: (e: FormEvent<HTMLFormElement>) => void;
   setOwnerName: (ownerName: string) => void;
   setOwnerEmail: (ownerEmail: string) => void;
+  destination: string;
+  eventStartNEndDates: DateRange | undefined;
 }
 
 export function ConfirmTripModal({
@@ -14,7 +18,16 @@ export function ConfirmTripModal({
   createTrip,
   setOwnerName,
   setOwnerEmail,
+  destination,
+  eventStartNEndDates,
 }: ConfirmTripModalProps) {
+  const displayedDate =
+    eventStartNEndDates && eventStartNEndDates.from && eventStartNEndDates.to
+      ? format(eventStartNEndDates.from, "d' de 'LLL")
+          .concat(" até ")
+          .concat(format(eventStartNEndDates.to, "d' de 'LLL"))
+      : null;
+
   return (
     <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
       <div className="w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
@@ -31,11 +44,11 @@ export function ConfirmTripModal({
           <p className="text-zinc-400 text-sm ">
             Para concluir a criação da viagem para{" "}
             <strong className="font-semibold text-zinc-100">
-              Florianópolis, Brasil
+              {destination}
             </strong>{" "}
             nas datas de{" "}
             <strong className="font-semibold text-zinc-100">
-              16 a 28 de Agosto de 2024
+              {displayedDate}
             </strong>{" "}
             preencha seus dados abaixo:
           </p>
@@ -54,7 +67,7 @@ export function ConfirmTripModal({
           </div>
 
           <div className="h-14 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
-            <User className="size-5 text-zinc-400" />
+            <AtSign className="size-5 text-zinc-400" />
             <input
               type="email"
               name="email"
